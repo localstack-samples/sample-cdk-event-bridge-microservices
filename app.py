21366d46600ed50076a9590295bb8c2084f5f4b0
Maximilian Hoheiser <max.hoheiser@gmail.com>
2024-06-03 19:03:34 +0200

    feat: add cdk app

---
+++ app.py
@@ -0,0 +1,25 @@
+import aws_cdk as cdk
+import os
+from events_stack_primary_region import EventsStackPrimaryRegion
+from events_stack_secondary_region import EventsStackSecondaryRegion
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ACCOUNT_ID = os.getenv("ACCOUNT_ID")
+REGION_PRIMARY = os.getenv("REGION_PRIMARY")
+REGION_SECONDARY = os.getenv("REGION_SECONDARY")
+
+
+app = cdk.App()
+
+EventsStackPrimaryRegion(
+    app, "EventsStackPrimaryRegion", env=cdk.Environment(region=REGION_PRIMARY)
+)
+EventsStackSecondaryRegion(
+    app,
+    "EventsStackSecondaryRegion",
+    env=cdk.Environment(region=REGION_SECONDARY),
+)
+
+app.synth()
